7ba06554fb3671097a6b7becea7d5ff62d93a696
LemanArt <leman210101@gmail.com>
2021-10-27 10:25:16 +0700

    menambahkan file latihan.py

---
+++ latihan.py
@@ -0,0 +1,19 @@
+print("Hello")
+print("Saya sedang belajar Python")
+
+a = 8
+b = 6
+print("variable a = ",a)
+print("variable b = ",b)
+print("hasil penjumlahan a+b= ", a+b,"\n")
+
+a = int(input("masukkan nilai a :"))
+b = int(input("masukkan nilai b :"))
+print("variable a = ",a)
+print("variable b = ",b)
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b), "\n")
+#Konversi nilai variable
+a = int(input("masukkan nilai a:"))
+b = int(input("masukkan nilai b:"))
+print("hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
