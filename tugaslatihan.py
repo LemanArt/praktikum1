732b839945f329ccd6351de7c9fc5c0f7538d75e
LemanArt <leman210101@gmail.com>
2021-10-28 00:38:50 +0700

    menambahkan file tugaslatihan.py

---
+++ tugaslatihan.py
@@ -0,0 +1,19 @@
+print("=====INPUT BIODATA DIRI=====\n")
+
+nama =input("nama:")
+call =input("panggilan:")
+nim =input("NIM:")
+tempatLahir =input("ttl:")
+umur =input("umur:")
+alamat =input("alamat:")
+telepon =input("telepon:")
+print("\n")
+
+print("Assalamu'alaikum Wr.Wb.,\n")
+
+print("Let me introduce my self. My Name is ",nama,", but you can call me ",call,"."
+"My NPM is ",nim,". i was born in ",tempatLahir,"and i am",umur,"years old. "
+"i am very glad if you want to inivite my house in ",alamat,".so, don't forget to call me"
+"before with the number",telepon,".\n\n")
+
+print ("thank you")
